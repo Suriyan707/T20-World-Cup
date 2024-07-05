79bb73167b900119a75cf4d12ef746fe997dacaa
DT_Suriya <suriya.krishnakumar@dispatchtrack.com>
2024-07-05 11:42:51 +0530

    complete Email Id validate

--- public/Script/Email id validate.rb
+++ public/Script/Email id validate.rb
@@ -2,4 +2,4 @@ Pattern = /^[A-Za-z0-9+_.-]+@([A-Za-z0-9]+\.)+[A-Za-z]{2,6}$/
 def email_valid? email
     email =~ Pattern
 end
-puts email_valid?("suriyan@gmail.com") ? "Email valid" : "Email invalid"
+puts email_valid?("suriyan@gmail.com") ? "True" : "False"
